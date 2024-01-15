4a3101c4f35073465d0b8f43e1a3cdfd09b2c7d9
AGHILAS <halouane.aghilas@yahoo.com>
2024-01-15 15:58:25 +0100

    ajout du style(scss)

---
+++ src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A PORPOS</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ab
+        assumenda. Voluptas, assumenda qui maxime impedit ad laborum perferendis
+        non quod, saepe, ut sapiente perspiciatis repellendus ratione ducimus ex
+        expedita nisi molestias modi voluptate magni praesentium. Mollitia amet
+        hic sequi, ullam perspiciatis similique commodi officia assumenda
+        expedita veniam aperiam dicta nobis, doloremque exercitationem enim
+        facere repellendus nihil. Dicta fuga possimus ipsa libero obcaecati
+        autem distinctio unde? Laborum nostrum vero eos quisquam, quasi itaque
+        perspiciatis excepturi voluptas quo iusto reiciendis cum magni vitae
+        aliquid eius minus placeat ipsum deserunt debitis, totam illum sed?
+        Similique pariatur dolorem, possimus sint ad explicabo obcaecati!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        itaque officiis laudantium error consectetur modi hic illo fugit
+        deserunt numquam veniam necessitatibus suscipit dicta dolor maxime,
+        deleniti, perspiciatis minima iste expedita! Cumque iusto autem
+        explicabo accusantium neque. Expedita modi laborum quisquam enim
+        molestiae at dolores ab error exercitationem earum corporis alias natus
+        maxime quae amet maiores aspernatur labore omnis ipsum ullam, quaerat
+        pariatur repudiandae! Asperiores tenetur, similique illum eius, culpa
+        cum eos, voluptas sed tempora ex dolor in. Perspiciatis officiis
+        consequuntur eos quo obcaecati! Omnis vel dicta ipsam alias nulla
+        aspernatur voluptate eveniet harum molestiae earum ipsa unde, hic
+        recusandae qui officia labore tempore nesciunt perferendis in quia
+        adipisci. Eum, quasi corrupti doloribus veniam deleniti delectus
+        voluptate expedita dolorum similique placeat. Dolorem aliquid illo nobis
+        ducimus fuga accusantium nisi reprehenderit officiis. Voluptate aliquam
+        explicabo adipisci quis architecto suscipit quam. Sit est provident,
+        nisi quis delectus modi impedit! Maiores itaque ab, fugiat
+        necessitatibus expedita esse, omnis dolor temporibus voluptatum
+        voluptates inventore neque, saepe perspiciatis facilis soluta nostrum
+        doloribus pariatur? Vel pariatur illo sapiente officia id inventore non
+        voluptatibus cum, reprehenderit, quae accusamus, alias autem sed.
+        Aliquid animi nulla reprehenderit vel nesciunt quidem accusamus possimus
+        esse optio? Cumque beatae deserunt ullam. Corrupti?
+      </p>
+    </div>
+  );
+};
+
+export default About;
